fcad581ada1d14cadca28a06281f892787e8cc8d
roman kostyrin <enemy-iubip@mail.ru>
2021-08-06 22:30:35 +0300

    fit: funish the task, all tests 100%

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix) {
+        return [];
+    }
+    let mat = [];
+    matrix.forEach((el, index) => {
+        if (index % 2 === 0) {
+            mat.push(...el);
+        } else {
+            mat.push(...el.reverse());
+        }
+    });
+    return mat;
+};
